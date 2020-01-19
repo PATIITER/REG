7b9e5ca27d5e3ac50f689d21c53b49802f7ada16
PATTER <patercup@gmail.com>
2020-01-19 23:13:22 +0700

    sdfsdf

--- DemoPatterApi/Controllers/TeacherController.cs
+++ DemoPatterApi/Controllers/TeacherController.cs
@@ -14,16 +14,16 @@ namespace DemoPatterApi.Controllers
     {
          public static List<Class> DataClass = new List<Class>
         {
-            new Class { IdClass = "1", NameClass = "admin1" },
-            new Class { IdClass = "2", NameClass = "admin1" },
-            new Class { IdClass = "3", NameClass = "admin1" },
+            new Class { IdClass = "001", NameClass = "Business Administration" },
+            new Class { IdClass = "002", NameClass = "Computer Engineering" },
+            new Class { IdClass = "003", NameClass = "International Business" },
 
         };
         public static List<Teacher> DataTeacher = new List<Teacher>
         {
-            new Teacher { IdTeacher = "1", NameTeacher = "admin1",Class = DataClass.ToArray(),Username = "admin1", Password = "12356"},
-            new Teacher { IdTeacher = "2", NameTeacher = "admin1",Class = DataClass.ToArray(),Username = "admin2", Password = "12356" },
-            new Teacher { IdTeacher = "3", NameTeacher = "admin1",Class = DataClass.ToArray(),Username = "admin3", Password = "12356" },
+            new Teacher { IdTeacher = "1", NameTeacher = "sdf",Class = DataClass.ToArray(),Username = "admin1", Password = "12356"},
+            new Teacher { IdTeacher = "2", NameTeacher = "sdtrwgg",Class = DataClass.ToArray(),Username = "admin2", Password = "12356" },
+            new Teacher { IdTeacher = "3", NameTeacher = "wtdfg",Class = DataClass.ToArray(),Username = "admin3", Password = "12356" },
 
         };
 
@@ -39,31 +39,81 @@ namespace DemoPatterApi.Controllers
             return DataTeacher.FirstOrDefault(it => it.IdTeacher == id.ToString());
         }
 
+         [HttpGet("{id}")]
+        public ActionResult<Teacher> GetTeacherId( string id )
+        {
+            return DataTeacher.FirstOrDefault(it => it.Username == id.ToString());
+        }
+
         [HttpPost]
         public Teacher AddTeacher([FromBody] Teacher Teacherx)
         {
             var id = Guid.NewGuid().ToString();
             var item = new Teacher
             {
-                IdTeacher = id,
-                NameTeacher = Teacherx.NameTeacher
-               
+                IdTeacher = id.ToString(),
+                NameTeacher = Teacherx.NameTeacher,
                 
+                Username = Teacherx.Username,
+                Password = Teacherx.Password
+
             };
 
             DataTeacher.Add(item);
             return item;
         }
 
+         
+                    //เพิ่มวิชาในอาจาร
+         [HttpPut("{id}")]
+        public Teacher AddTeacherClass(string id, [FromBody] Class Classx)
+        {
+            var data = DataTeacher.FirstOrDefault(it => it.IdTeacher == id.ToString());
+
+
+            var AA =data.Class.ToList();
+             Console.WriteLine(data.Class.ToList());
+
+             var item =new Class{
+
+                 IdClass =Classx.IdClass,
+                 NameClass = Classx.NameClass
+
+             };
+
+             AA.Add(item);
+             Console.WriteLine(AA.ToList());
+           
+
+            var item2 = new Teacher
+            {
+                IdTeacher = id.ToString(),
+                NameTeacher = data.NameTeacher,
+                Class = AA.ToArray(),
+                Username = data.Username,
+                Password = data.Password
+
+                
+            };
+            DataTeacher.Remove(data);
+            DataTeacher.Add(item2);
+            return item2;
+
+        }
+        
+
         [HttpPut("{id}")]
-        public Teacher EditStudent(string id, [FromBody] Teacher Teacherx)
+        public Teacher EditTeacher(string id, [FromBody] Teacher Teacherx)
         {
             var _id = DataTeacher.FirstOrDefault(it => it.IdTeacher == id.ToString());
             var item = new Teacher
             {
                 IdTeacher = id,
-                NameTeacher = Teacherx.NameTeacher
-               
+                NameTeacher = Teacherx.NameTeacher,
+                Class = Teacherx.Class,
+                Username = Teacherx.Username,
+                Password = Teacherx.Password
+
                 
             };
             DataTeacher.Remove(_id);
@@ -71,6 +121,7 @@ namespace DemoPatterApi.Controllers
             return item;
 
         }
+        
 
         [HttpDelete("{id}")]
         public void DeleteTeacher(string id)
@@ -78,6 +129,9 @@ namespace DemoPatterApi.Controllers
             var delete = DataTeacher.FirstOrDefault(it => it.IdTeacher == id.ToString());
             DataTeacher.Remove(delete);
         }
+
+
+       
 
 
     }
